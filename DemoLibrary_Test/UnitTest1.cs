49d28ada5ac33e090ab129bad4393d29371f3c99
Bastien Malea <bastmalea@gmail.com>
2022-05-04 09:17:33 +0200

    [DEV] Ajout Dossier de test fonctionnels

---
+++ DemoLibrary_Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace DemoLibrary_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+
+        public void Test2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
